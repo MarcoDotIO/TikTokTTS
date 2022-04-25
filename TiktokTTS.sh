8fd3b344ae7cda9229c1d65ab347f25cd1157ae2
MarcoDotIO <Marcus.Arnett10@gmail.com>
2022-04-25 14:29:54 -0400

    Add Initial Shell File

---
+++ TiktokTTS.sh
@@ -0,0 +1,17 @@
+STR=""
+
+for word in "$@"
+do
+    STR+=${word}
+    STR+="%20"
+done
+
+echo ${STR}
+
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     curl -s -X POST 'https''://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_002&req_text=HelloWorld'| jq .data.v_str | base64 -di | ffplay -autoexit -volume 10 -f mp3 -;;
+    Darwin*)    curl -s -X POST 'https''://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_002&req_text='${STR} | jq .data.v_str | gbase64 -di | ffplay -autoexit -volume 10 -f mp3 -;;
+    *)          echo "THIS MACHINE DOES NOT SUPPORT THIS SCRIPT!!!;;"
+esac
