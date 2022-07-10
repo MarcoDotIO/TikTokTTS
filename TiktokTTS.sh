e89170d1d83ba6c75049fef5449fb1ed5548a6ea
MarcoDotIO <Marcus.Arnett10@gmail.com>
2022-07-10 02:54:57 -0400

    Fix issue with Linux invocation
    Changed from hard coded string of "HelloWorld" to the user's input.

--- TiktokTTS.sh
+++ TiktokTTS.sh
@@ -9,7 +9,7 @@ done
 unameOut="$(uname -s)"
 
 case "${unameOut}" in
-    Linux*)     curl -s -X POST 'https''://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_002&req_text=HelloWorld'| jq .data.v_str | base64 -di | ffplay -autoexit -volume 10 -f mp3 -;;
+    Linux*)     curl -s -X POST 'https''://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_002&req_text='${STR} | jq .data.v_str | base64 -di | ffplay -autoexit -volume 10 -f mp3 -;;
     Darwin*)    curl -s -X POST 'https''://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_002&req_text='${STR} | jq .data.v_str | gbase64 -di | ffplay -autoexit -volume 10 -f mp3 -;;
     *)          echo "THIS MACHINE DOES NOT SUPPORT THIS SCRIPT!!!;;"
 esac
